c93b1079e54a68926c9d0736ef2248adff266da0
すえ <souhait.0614@gmail.com>
2023-06-09 09:16:35 +0900

    なんか色々した

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { readFile } from "fs/promises"
+
+import { serve } from "@hono/node-server"
+import { Hono } from "hono"
+
+const html = readFile("src/index.html")
+
+const routePath = {
+  root: "/",
+  cjp: "/cjp",
+  genhera: "/genhera",
+  nomlish: "/nomlish",
+  googleRetranslate: "/googleRetranslate",
+}
+
+// const setHeader = (c: Context) => {
+//   c.header("Access-Control-Allow-Origin", "*")
+// }
+
+const app = new Hono()
+app.get(routePath.root, async (c) => c.html((await html).toString()))
+
+// app.post(routePath.cjp, (c) => {
+//   try {
+//     const input = c.req.json()
+//   } catch (error) {
+
+//   }
+// })
+
+serve(app)
